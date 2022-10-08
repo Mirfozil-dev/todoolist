9690e29ccebac63702256131b2dce88e1207af73
Mirfozil-dev <makhmudov03@icloud.com>
2022-10-08 21:07:31 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <form-component :query="query" @onInput="onInput" @onSave="addTask"/>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="deleteTask(index)">delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import formComponent from './components/formComponent'
+
+export default {
+  name: 'App',
+  components: {
+    formComponent
+  },
+  data() {
+    return {
+      query: '',
+      tasks: []
+    }
+  },
+  methods: {
+    onInput(query) {
+      this.query = query
+    },
+    addTask() {
+      this.tasks.push(this.query)
+      this.query = ''
+      console.log(this.tasks)
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/formComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" :value="query" @input="inputQuery" @keyup.enter="$emit('onSave')">
+    <button @click="$emit('onSave')">save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "formComponent",
+  props: ['query'],
+  methods: {
+    inputQuery(event) {
+      this.$emit('onInput', event.target.value)
+      // console.log(event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
